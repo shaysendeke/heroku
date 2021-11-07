f28067f69baf24cc6c4b9e0555b7549575c7dc06
shaysendeke <77267283+shaysendeke@users.noreply.github.com>
2021-11-07 10:45:18 +0200

    heroku repository

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("app is loading")
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.get('*', (req, res) =>{
+    res.send("hello world")
+})
+
+app.listen(PORT)
+console.log(`app is listening on port ${PORT}`);
